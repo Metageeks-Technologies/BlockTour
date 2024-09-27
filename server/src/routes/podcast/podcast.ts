bc3142bb577ec32f134ebb5938426fb35106489a
hariom-verma-meta <hariomverma@metageeks.tech>
2024-09-27 10:06:36 +0530

    podcast

--- server/src/routes/podcast/podcast.ts
+++ server/src/routes/podcast/podcast.ts
@@ -1,4 +1,4 @@
-import {createPodcast, deletePodcast, getAllPodcasts, getPodcastById, updatePodcast} from '../../controllers/podcastController';
+import {createPodcast, deletePodcast, getAllPodcasts, getPodcastById, getPodcastByPermaLink, updatePodcast} from '../../controllers/podcastController';
 import express from 'express';
 
 const podcastRouter = express.Router();
@@ -8,5 +8,6 @@ podcastRouter.get('/all-podcasts', getAllPodcasts);
 podcastRouter.get('/podcasts/:id', getPodcastById);
 podcastRouter.put('/podcasts/:id', updatePodcast);
 podcastRouter.delete('/podcasts/:id', deletePodcast);
+podcastRouter.get('/podcast/:permalink', getPodcastByPermaLink);
 
 export default podcastRouter;
